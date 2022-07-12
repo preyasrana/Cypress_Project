bdcc8453a540d3f40efdd645f1ed770c1535d970
preyas <preyas.rana@zymr.com>
2022-07-12 12:31:30 +0530

    test

---
+++ cypress/e2e/Cypress-framework/cy_framework1.js
@@ -0,0 +1,37 @@
+/// <reference types ="Cypress"/>
+
+describe('test suite', function() {
+    
+    before(() => {
+        // root-level hook runs once before all tests
+
+        cy.fixture('register_practice').then(function(data) {
+
+            this.data = data
+
+        })
+    })
+
+    it('first test case', function() {
+
+        cy.viewport(1366, 768)
+        cy.visit('https://www.rahulshettyacademy.com/angularpractice/')
+
+        cy.get('input[name="name"]:nth-child(2)').type(this.data.name)
+        cy.get('select[id="exampleFormControlSelect1"]').select(this.data.gender)
+
+
+        cy.get('input[name="name"]:nth-child(1)').should('have.value',this.data.name)
+        //attribute for validation
+        cy.get('input[name="name"]:nth-child(2)').should('have.attr','minlength','2')
+
+        cy.get('input[id="inlineRadio3"]').should('be.disabled')
+        
+
+
+
+
+    })
+
+
+})
